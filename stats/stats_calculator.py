93bc46b410644e437d5ec926778931aae16a3e16
Kristofferson Culmer <krcd58@mail.missouri.edu>
2022-06-21 11:55:51 -0500

    Add files via upload

--- stats/stats_calculator.py
+++ stats/stats_calculator.py
@@ -1,4 +1,5 @@
 import os.path
+from statistics import median
 
 def get_file_name():
     while True:
@@ -30,6 +31,28 @@ def load_numbers_list(file_name):
     #return the list
     return numbers_list
 
+def calculate_median_value(numbers_list):
+    #sort the list
+    numbers_list.sort()
+    #determine if the list has an even or odd number of items using the len() function
+    if (len(numbers_list) % 2 == 0):
+        is_even = True
+    else:
+        is_even = False
+    #if even find the two middle values and calculate the average of the two
+    if is_even:
+        index_1 = int(len(numbers_list) / 2)
+        print(index_1)
+        index_2 = index_1 - 1
+        print(index_2)
+        median = (numbers_list[index_1] + numbers_list[index_2]) / 2
+    #if odd, return the middle value
+    else:
+        index = int((len(numbers_list) - 1) / 2)
+        median = numbers_list[index]
+ 
+    return median
+
 
 def main():
     #ask user to enter filename and open that file
@@ -38,7 +61,15 @@ def main():
     #load number from file into a list
     list_of_numbers = load_numbers_list(file_name)
 
-    print(list_of_numbers)
+    #check the length of the list
+    if len(list_of_numbers) == 0:
+        print("File was empty")
+    elif len(list_of_numbers) == 1:
+        print("Only one number in file")
+    else:
+        median = calculate_median_value(list_of_numbers)
+        print(median)
+
 
 
 main()
