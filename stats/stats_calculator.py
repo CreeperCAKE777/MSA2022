59079b65e8f6c630177e519c42ef58b578f709ac
Kristofferson Culmer <krcd58@mail.missouri.edu>
2022-06-21 09:06:39 -0500

    Add files via upload

---
+++ stats/stats_calculator.py
@@ -0,0 +1,39 @@
+def get_file_name():
+    while True:
+        #prompt user to enter file name
+        user_input = input("Please enter a file name: ")
+        #make sure user entered a value. Re prompt if not
+        if user_input == "":
+            print("ERROR: please enter a value.\n")
+            continue
+        else:
+            break
+    #else return the value
+    return user_input
+
+#Function to load numbers from file into list
+#Parameters: file name
+#returns a list of numbers
+def load_numbers_list(file_name):
+    numbers_list = []
+    #open the file
+    file = open(file_name, "r")
+    #read file line by line
+    for number in file:
+        #convert to int and append numbers to a list
+        numbers_list.append(int(number))
+    #return the list
+    return numbers_list
+
+
+def main():
+    #ask user to enter filename and open that file
+    file_name = get_file_name()
+    
+    #load number from file into a list
+    list_of_numbers = load_numbers_list(file_name)
+
+    print(list_of_numbers)
+
+
+main()
