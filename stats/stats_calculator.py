27541fdfcb715afa80993883a9a884637c1e4414
Kristofferson Culmer <krcd58@mail.missouri.edu>
2022-06-23 08:55:40 -0500

    Add files via upload

--- stats/stats_calculator.py
+++ stats/stats_calculator.py
@@ -1,5 +1,4 @@
 import os.path
-from statistics import median
 
 def get_file_name():
     while True:
@@ -45,31 +44,88 @@ def calculate_median_value(numbers_list):
         print(index_1)
         index_2 = index_1 - 1
         print(index_2)
-        median = (numbers_list[index_1] + numbers_list[index_2]) / 2
+        median_value = (numbers_list[index_1] + numbers_list[index_2]) / 2
     #if odd, return the middle value
     else:
         index = int((len(numbers_list) - 1) / 2)
-        median = numbers_list[index]
+        median_value = numbers_list[index]
  
-    return median
+    return median_value
+
+def determine_modal_value(numbers_list):
+    #create a dictionary to store list vaules and frequencies
+    modal_values = {}
+    list_of_modal_values = []
+    #Loop through list.
+    for number in numbers_list:
+    #   If the number is in the dictionary then add 1 to its value
+    #   If number not in dictionary then insert as key in dict with value 1
+        if number in modal_values:
+            modal_values[number] += 1
+        else:
+            modal_values[number] = 1
+    #Determine the largest value in the dictionary
+    list_values = modal_values.values()
+    max_value = max(list_values)
+    #Append those keys with the largest values to a list
+    for key in modal_values:
+        if modal_values[key] == max_value:
+            list_of_modal_values.append(key)
+    #Return list of modal value(s)
+    return list_of_modal_values
+    
+def calculate_sum(numbers_list):
+    total = 0
+    for number in numbers_list:
+        total += number
+    return total
+
+def print_output(name_of_file, list_sum, num_items, list_avg, max_val, min_val, num_range, median_val, modal_val):
+    print("\nProgram Statistics\n-------------------")
+    print(f"Filename: {name_of_file}")
+    print(f"Sum: {list_sum}")
+    print(f"Count: {num_items}")
+    print(f"Average: {list_avg:.2f}")
+    print(f"Maximum: {max_val}")
+    print(f"Minimum: {min_val}")
+    print(f"Range: {num_range}")
+    print(f"Median: {median_val}")
+    print(f"Mode: {modal_val}")
+    return
+
+def process_data(file_name, list_of_numbers):
+    median_value = calculate_median_value(list_of_numbers)
+    modal_values = determine_modal_value(list_of_numbers)
+    num_items = len(list_of_numbers)
+    sum_of_list = calculate_sum(list_of_numbers)
+    average = sum_of_list / num_items
+    max_value = max(list_of_numbers)
+    min_value = min(list_of_numbers)
+    range_of_values = max_value - min_value
+    print_output(file_name, sum_of_list, num_items, average, max_value, min_value, range_of_values, median_value, modal_values)
+    return
 
 
 def main():
-    #ask user to enter filename and open that file
-    file_name = get_file_name()
-    
-    #load number from file into a list
-    list_of_numbers = load_numbers_list(file_name)
+    while True:
+        #ask user to enter filename and open that file
+        file_name = get_file_name()
+        
+        #load number from file into a list
+        list_of_numbers = load_numbers_list(file_name)
 
-    #check the length of the list
-    if len(list_of_numbers) == 0:
-        print("File was empty")
-    elif len(list_of_numbers) == 1:
-        print("Only one number in file")
-    else:
-        median = calculate_median_value(list_of_numbers)
-        print(median)
+        #check the length of the list to determine how to process the data
+        if len(list_of_numbers) == 0:
+            print("File was empty!")
+        elif len(list_of_numbers) == 1:
+            print_output(file_name, list_of_numbers[0], 1, list_of_numbers[0], list_of_numbers[0], 
+            list_of_numbers[0], 0, list_of_numbers[0], list_of_numbers[0])
+        else:
+            process_data(file_name, list_of_numbers)
 
-
-
+        #Ask user if they want to perform another calculation. If not, end program.
+        do_again = input("\nWould you like to perform another calculation? Press 'Y' or 'y' to continue: ")
+        if do_again.upper() != "Y":
+            print("Goodbye :-)")
+            break
 main()
